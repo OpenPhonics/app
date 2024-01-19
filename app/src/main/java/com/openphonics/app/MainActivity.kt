1d35ebcb742364a16fe63180bf0d281265404929
Advait Vedant <advait.suraj.vedant@gmail.com>
2024-01-19 15:02:56 -0800

    added splash screen with animation

--- app/src/main/java/com/openphonics/app/MainActivity.kt
+++ app/src/main/java/com/openphonics/app/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.openphonics.app
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,11 +13,38 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.openphonics.app.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
+        splashScreen.setOnExitAnimationListener { screen ->
+            val zoomX = ObjectAnimator.ofFloat(
+                screen.iconView,
+                View.SCALE_X,
+                0.4f,
+                0.0f
+            )
+            zoomX.interpolator = OvershootInterpolator()
+            zoomX.duration = 500L
+            zoomX.doOnEnd { screen.remove() }
+
+            val zoomY = ObjectAnimator.ofFloat(
+                screen.iconView,
+                View.SCALE_Y,
+                0.4f,
+                0.0f
+            )
+            zoomY.interpolator = OvershootInterpolator()
+            zoomY.duration = 500L
+            zoomY.doOnEnd { screen.remove() }
+
+            zoomX.start()
+            zoomY.start()
+        }
         setContent {
             AppTheme {
                 // A surface container using the 'background' color from the theme

6c09069ac873c1cca84dd009fc4f4bf620c48481
Advait Vedant <advait.suraj.vedant@gmail.com>
2024-02-26 15:11:41 -0800

    removed theme setup from app module

--- app/src/main/java/com/advaitvedant/openphonics/MainActivity.kt
+++ app/src/main/java/com/advaitvedant/openphonics/MainActivity.kt
@@ -9,21 +9,16 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.advaitvedant.openphonics.ui.theme.OpenphonicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            OpenphonicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                Greeting("Android")
             }
         }
     }
@@ -35,12 +30,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OpenphonicsTheme {
-        Greeting("Android")
-    }
 }

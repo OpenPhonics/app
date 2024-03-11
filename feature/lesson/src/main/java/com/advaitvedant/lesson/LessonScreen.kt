0979d408cb6021738d8679054725b976158ff0f1
Advait Vedant <advait.suraj.vedant@gmail.com>
2024-03-11 05:26:00 -0700

    fixed speech to text by adding query to android to manifest

--- feature/lesson/src/main/java/com/advaitvedant/lesson/LessonScreen.kt
+++ feature/lesson/src/main/java/com/advaitvedant/lesson/LessonScreen.kt
@@ -3,13 +3,38 @@ package com.advaitvedant.lesson
 import android.Manifest
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MinorCrash
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -17,15 +42,17 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 fun LessonRoute(
     viewModel: LessonViewModel = hiltViewModel(),
     onBackClick: () -> Unit){
-    Column(modifier = Modifier.fillMaxSize()){
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment =  Alignment.CenterHorizontally){
         val loading = viewModel.loading.collectAsStateWithLifecycle()
         if (loading.value) {
-            Button(onClick = viewModel::playSound) {
+            Button(onClick = viewModel::reset) {
                 Text("CLICK ME")
             }
+            RoundedHoldIconButton(size = 0.5f, icon = Icons.Default.MinorCrash, enabled = true, onPressed = viewModel::start, onReleased = viewModel::stop)
         } else {
             Text("LODAING")
         }
+        
         val context = LocalContext.current
         val multiplePermissionsLauncher = rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestMultiplePermissions()) { permission ->
             permission.entries.forEach {
@@ -43,5 +70,57 @@ fun LessonRoute(
         }) {
 
         }
+    }
+}
+
+@Composable
+fun RoundedHoldIconButton(
+    modifier: Modifier = Modifier,
+    size: Float,
+    icon: ImageVector,
+    enabled: Boolean,
+    backgroundColor: Color = MaterialTheme.colorScheme.inverseOnSurface,
+    iconColor: Color = MaterialTheme.colorScheme.background,
+    onPressed: () -> Unit,
+    onReleased: () -> Unit
+) {
+    var isPressed by remember { mutableStateOf(false) }
+
+    // Animate the button size
+    val buttonSize by animateFloatAsState(
+        targetValue = if (isPressed) size * 0.9f else size,
+        animationSpec = spring(stiffness = Spring.StiffnessHigh)
+    )
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth(buttonSize)
+            .aspectRatio(1f)
+            .clip(CircleShape)
+            .border(3.dp, iconColor, CircleShape)
+            .clip(CircleShape)
+            .background(backgroundColor)
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onPress = {
+                        if (enabled) {
+                            isPressed = true
+                            onPressed()
+                            awaitRelease()
+                            isPressed = false
+                            onReleased()
+                        }
+                    }
+                )
+            },
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            modifier = Modifier
+                .fillMaxSize(0.75f),
+            imageVector = icon,
+            contentDescription = null,
+            tint = if (enabled) iconColor else iconColor.copy(0.5f)
+        )
     }
 }
--- feature/lesson/src/main/java/com/advaitvedant/lesson/LessonViewModel.kt
+++ feature/lesson/src/main/java/com/advaitvedant/lesson/LessonViewModel.kt
@@ -1,6 +1,7 @@
 package com.advaitvedant.lesson
 
 import android.content.Context
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -8,6 +9,7 @@ import com.advaitevdant.data.repository.DataRepository
 import com.advaitvedant.audioplayer.SoundManager
 import com.advaitvedant.audioplayer.SoundPlayer
 import com.advaitvedant.model.LessonWithData
+import com.advaitvedant.speechtotext.SpeechToTextParser
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.flow.SharingStarted
@@ -23,51 +25,48 @@ class LessonViewModel @Inject constructor(
     val data: DataRepository,
     val soundManager: SoundManager,
     val soundPlayer: SoundPlayer,
+    val stt: SpeechToTextParser,
     @ApplicationContext val context: Context,
     savedStateHandle: SavedStateHandle
 ) : ViewModel() {
+    private val sttState = stt.state
     private val lessonArgs: LessonArgs = LessonArgs(savedStateHandle)
-    init {
-        viewModelScope.launch {
-            soundManager.downloadBatch(
-                lessonId,
-                listOf(
-                    Pair(
-                        "then.mp3",
-                        "https://storage.googleapis.com/openphonics.appspot.com/words/en/Then.mp3"
-                    )
-                )
-            )
-        }
-        val dir = File(context.filesDir, lessonId.toString())
-
-        soundPlayer.preload(File(dir, "then.mp3").absolutePath)
-
-        soundPlayer.preload(R.raw.then)
-    }
+    private val enmp3: (String) -> String= {text -> "https://storage.googleapis.com/openphonics.appspot.com/words/en/$text" }
+    private val tamp3: (String) -> String= {text -> "https://storage.googleapis.com/openphonics.appspot.com/words/ta/$text" }
+    private val dir = File(context.filesDir, lessonId.toString()).absolutePath
     private val lessonId
         get() = lessonArgs.lesson
     val loading: StateFlow<Boolean>
         get() = soundPlayer.loadedAllSounds
-    fun playSound(){
-        val dir = File(context.filesDir, lessonId.toString())
-        viewModelScope.launch {
-            soundPlayer.enqueue(File(dir, "then.mp3").absolutePath)
-//            delay(500L)
-            soundPlayer.enqueue(File(dir, "then.mp3").absolutePath)
-//            delay(500L)
-            soundPlayer.enqueue(File(dir, "then.mp3").absolutePath)
-//            delay(500L)
-            soundPlayer.enqueue(File(dir, "then.mp3").absolutePath)
-        }
 
-        soundPlayer.enqueue(R.raw.then)
-        soundPlayer.enqueue(R.raw.then)
+    init {
+        viewModelScope.launch {
+            sttState.collect {
+                Log.d("SPEECHTOTETX", sttState.value.text)
+            }
+        }
+    }
+    fun start(){
+        stt.startListening()
+    }
+    fun stop(){
+        stt.stopListening()
     }
 
+    fun reset(){
+        stt.resetText()
+    }
     val lessonState: StateFlow<LessonUiState> =
         data.lesson(lessonId)
-            .map(LessonUiState::Success)
+            .map {
+                soundManager.downloadBatch(lessonId, it.words.map { word -> Pair(word.text+".mp3", enmp3(word.text))})
+                soundManager.downloadBatch(lessonId, it.words.map { word -> Pair(word.translation+".mp3", tamp3(word.translation))})
+                it.words.forEach { word ->
+                    soundPlayer.preload("$dir/${word.text}.mp3")
+                    soundPlayer.preload("$dir/${word.translation}.mp3")
+                }
+                LessonUiState.Success(it)
+            }
             .stateIn(
                 scope = viewModelScope,
                 started = SharingStarted.WhileSubscribed(5_000),
